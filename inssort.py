98811daa56504bb6cf77d58a9a60a62054769b6c
Alvin Ho <alvinho@yandex.com>
2022-11-21 21:48:28 +0800

    insertion sort initial commit

---
+++ inssort.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+#
+# This program uses an index sort method
+# to put an randomly ordered list into a sorted 
+#
+
+household = []
+
+def GetObj():
+# This procedue reads from the input file, objectlist.text
+# & populates the list household
+#
+     source_file = open("objectlist.text", "r")
+     Organize = source_file.readlines()
+     i=0
+     for words in Organize :
+          household.append(words.rstrip("\r\n"))
+          i += 1
+
+     source_file.close()
+     return(household)
+
+household = GetObj()
